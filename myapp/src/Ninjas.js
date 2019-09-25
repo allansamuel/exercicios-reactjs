b9631bbcb0e0c92923374b263cc9e140d34ba453
allansamuel <allangomeseu@hotmail.com>
2019-09-25 16:22:06 -0300

    21 - adding css files

--- myapp/src/Ninjas.js
+++ myapp/src/Ninjas.js
@@ -1,15 +1,14 @@
 import React from 'react';
+import './ninjas.css'
 
 const Ninjas = ({ninjas, deleteNinja}) => {
     const ninjaList = ninjas.map(ninja => {
         return ninja.age > 20 ? (
-            <div key={ninja.id}>
+            <div class="ninja" key={ninja.id}>
                 <div>Name: {ninja.name}</div>
                 <div>Age: {ninja.age}</div>
                 <div>Belt: {ninja.belt}</div>
                 <button onClick={({/* () => {} faz com que a funçao não seja chamada diretamente, e sim quando o botão é clicado */})=>{deleteNinja(ninja.id)}}>Delete ninja</button>
-                <hr/>
-                <hr/>
             </div>
         ) : null
     })
