2d96ed84abfb9694977e4dd41bd33ef327f042d7
allansamuel <allangomeseu@hotmail.com>
2019-09-25 15:58:40 -0300

    19 - deleting data

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -21,12 +21,21 @@ class App extends Component {
     })
   }
 
+  deleteNinja = (id) => {
+    let ninjas = this.state.ninjas.filter(ninja => {
+      return ninja.id !== id //cria novo array filtrando apenas os ninjas que não tenham o id do parâmetro
+    })
+    this.setState({
+      ninjas: ninjas
+    })
+  }
+
   render(){
     return (
       <div className="App">
         <h1>My first react app</h1>
         {/* passa essa lista como props de Ninja */}
-        <Ninjas ninjas={this.state.ninjas}/> 
+        <Ninjas deleteNinja={this.deleteNinja} ninjas={this.state.ninjas}/> 
 
         <AddNinja addNinja={this.addNinja} />
 
