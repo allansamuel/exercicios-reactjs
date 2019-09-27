0f520eb8b06124fd3eded179db0d79f3947083ed
allansamuel <allangomeseu@hotmail.com>
2019-09-27 15:19:29 -0300

    23 - adding component & state

--- todoapp/src/App.js
+++ todoapp/src/App.js
@@ -1,13 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
+import Todos from './Todos'
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends Component {
+  state = {
+    todos: [
+      {id: 1, content: 'Pagar conta renner :P'},
+      {id: 2, content: 'Estudar para a prova de Matemática'}
+    ]
+  }
+
+  deleteToDo = (id) => {
+    const todos = this.state.todos.filter(todo =>{
+      return todo.id !== id
+    })
+    this.setState({
+      todos: todos
+    })
+  }
+
+  render() {
+    return (
+      <div className="todo-app container">
+        <h1 className="center blue-text">Tarefas</h1>
+        <Todos todos={this.state.todos} deleteToDo={this.deleteToDo}/>
+      </div>
+    );
+  }
 }
 
 export default App;
