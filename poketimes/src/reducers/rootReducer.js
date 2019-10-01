de58ca3b177f859790c3cdcd0484356779a5afc8
allansamuel <allangomeseu@hotmail.com>
2019-10-01 16:48:25 -0300

    41 - blog detail page

--- poketimes/src/reducers/rootReducer.js
+++ poketimes/src/reducers/rootReducer.js
@@ -1,8 +1,8 @@
 const initState = {
     posts: [
-        {id: '1', title: 'Bulbasaur', body: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Autem est itaque ipsam repudiandae odio veritatis nostrum aliquam corrupti quo esse molestiae tempora sit, voluptatem distinctio vitae ab accusamus sequi! Nobis?'},
-        {id: '2', title: 'Charmander', body: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Autem est itaque ipsam repudiandae odio veritatis nostrum aliquam corrupti quo esse molestiae tempora sit, voluptatem distinctio vitae ab accusamus sequi! Nobis?'},
-        {id: '3', title: 'Squirtle', body: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Autem est itaque ipsam repudiandae odio veritatis nostrum aliquam corrupti quo esse molestiae tempora sit, voluptatem distinctio vitae ab accusamus sequi! Nobis?'}
+        {id: '1', title: 'Bulbasaur', body: 'Bulbasaur can be seen napping in bright sunlight. There is a seed on its back. By soaking up the sun\'s rays, the seed grows progressively larger.'},
+        {id: '2', title: 'Charmander', body: 'The flame that burns at the tip of its tail is an indication of its emotions. The flame wavers when Charmander is enjoying itself. If the Pokémon becomes enraged, the flame burns fiercely.'},
+        {id: '3', title: 'Squirtle', body: 'Squirtle\'s shell is not merely used for protection. The shell\'s rounded shape and the grooves on its surface help minimize resistance in water, enabling this Pokémon to swim at high speeds.'}
     ]
 }
 
