dc700eaf63974abe96d898078dc01f5b0d1633e9
allansamuel <allangomeseu@hotmail.com>
2019-10-16 16:56:11 -0300

    20 - project details data

--- marioplan/src/components/projects/ProjectDetails.js
+++ marioplan/src/components/projects/ProjectDetails.js
@@ -1,23 +1,46 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import {firestoreConnect} from 'react-redux-firebase'
+import { compose } from 'redux'
 
 const ProjectDetails = (props) => {
-    const id = props.match.params.id
-    return (
+    const {project} = props;
+    if(project){
+        return(
         <div className="container sextion project-details">
             <div className="card z-depth-0">
                 <div className="card-content">
-                    <span className="card-title">Project Title = {id}</span>
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rerum assumenda magni ad at sint nostrum adipisci, incidunt odit modi! Perferendis a eius repellat voluptate nihil incidunt ut aut, hic sit?</p>
+                    <span className="card-title">{project.title}</span>
+                    <p>{project.content}</p>
                 </div>
             </div>
 
             <div className="card-action grey lighten-4 grey-text">
-                <div>Posted by Allan</div>
+                <div>Posted by {project.authotFirstName} {project.authotLastName}</div>
                 <div>2nd Oct, 16pm</div>
             </div>
-
         </div>
-    )
+        )
+    }else{
+        return (
+            <div className="container center">
+                <p>Loading project...</p>
+            </div>
+            )
+    }
+    
 }
-
-export default ProjectDetails
+const mapStateToProps = (state, ownProps) => {
+    const id = ownProps.match.params.id;
+    const projects = state.firestore.data.projects;
+    const project = projects ? projects[id] : null
+    return {
+        project
+    }
+}
+export default compose(
+    connect(mapStateToProps),
+    firestoreConnect([
+        {collection: 'projects'}
+    ])
+)(ProjectDetails)
