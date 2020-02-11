d0f283ad57af81dbe0a0acbd9498954e329cec10
allansamuel <allan.samuelg@gmail.com>
2020-02-11 10:15:37 -0300

    Organizing repo

---
+++ training/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter , Route } from 'react-router-dom';
+import {Home} from 
+function App() {
+  return (
+    <BrowserRouter>
+      <div>
+        <Route exact path="/" component={Home}/>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
