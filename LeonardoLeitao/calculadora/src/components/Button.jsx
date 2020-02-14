54222a926cda14194096a4343f672ad63a083008
allansamuel <allan.samuelg@gmail.com>
2020-02-14 10:43:28 -0300

    Display component

--- LeonardoLeitao/calculadora/src/components/Button.jsx
+++ LeonardoLeitao/calculadora/src/components/Button.jsx
@@ -2,5 +2,7 @@ import React from 'react';
 import './Button.css';
 
 export default (props) => (
-    <button className="button">{props.label}</button>
+    <button className="button">
+        {props.label}
+    </button>
 )
