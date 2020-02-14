8afd032a70a899c3efd94c780af1c623c8bd237c
allansamuel <allan.samuelg@gmail.com>
2020-02-14 13:14:04 -0300

    Working on functions

--- LeonardoLeitao/calculadora/src/main/Calculator.jsx
+++ LeonardoLeitao/calculadora/src/main/Calculator.jsx
@@ -4,26 +4,61 @@ import './Calculator.css';
 import Button from '../components/Button';
 import Display from '../components/Display'
 
+const initialState = {
+    displayValue: '0',
+    clearDisplay: false,
+    operation: null,
+    values: [0,0],
+    current: 0
+}
+
 export default class Calculator extends Component {
 
+    state = {
+        ...initialState
+    }
+
     clearMemory(){
-        console.log("limpa")
+        this.setState({ ...initialState})
     }
 
     setOperation(operation){
         console.log(operation)
     }
 
-    addDigit(n){
-        console.log(n)
+    addDigit = (n) =>{
+
+        //verificando se é possível adicionar um . na operação
+        if (n === '.' && this.state.displayValue.includes('.')){
+            return
+        }
+
+        //constante utilizada para verificar se o display está limpo
+        const clearDisplay = this.state.displayValue === '0' || this.state.clearDisplay
+
+        //se o display estiver vazio (zero), o valor apresentado não mudará ao clicar em zero. E mudará, caso clique em outro valor
+        const currentValue = clearDisplay ? '' : this.state.displayValue
+
+        const displayValue = currentValue + n
+
+        console.log(displayValue)
+
+        this.setState({displayValue, clearDisplay: false})
+
+        if(n !== '.'){
+            const i = this.state.current
+            const newValue = parseFloat(displayValue)
+            const values = [...this.state.values]
+            values[i] = newValue
+            this.setState({values})
+        }
     }
 
     render() {
  
-
         return (
             <div className="calculator">
-                <Display value={100} />
+                <Display value={this.state.displayValue} />
                 <Button label="AC" click={() => this.clearMemory()} triple />
                 <Button label="/" click={this.setOperation} operation  />
                 <Button label="7" click={this.addDigit}/>
